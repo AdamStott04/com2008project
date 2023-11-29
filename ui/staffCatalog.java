28b3b0c1481d2a1c144367c62f0508df7e21958a
Alfie <amarsh3@sheffield.ac.uk>
2023-11-29 02:00:24 +0000

    Staff catalog shows all items

--- ui/staffCatalog.java
+++ ui/staffCatalog.java
@@ -25,7 +25,10 @@ public class staffCatalog extends JFrame {
     private List<Item> allItemsInOrder;
     public staffCatalog(ResultSet items, User user, String category) throws SQLException {
         try {
+            //DefaultTableModel tableModel = (DefaultTableModel) itemsTable.getModel();
             DefaultTableModel tableModel = new DefaultTableModel();
+            //itemsTable = new JTable(tableModel);
+            itemsTable.setModel(tableModel);
 
             // Create columns based on ResultSet metadata
             tableModel.addColumn("Brand");
@@ -88,9 +91,9 @@ public class staffCatalog extends JFrame {
                 }
             }
 
-            itemsTable.setModel(tableModel);
             itemsTable.setDefaultEditor(Object.class, null);
             scrollPane.setViewportView(itemsTable);
+
             backButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
